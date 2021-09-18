de43936a6f763cfc1b1785d6b875fca1936eddd6
kensonhui <asimplekevin@gmail.com>
2021-09-18 01:09:03 -0400

    Add skeleton tracking

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,5 +1,11 @@
-import {useRef, useCallback } from "react";
+import {useRef, useEffect, useState, useCallback } from "react";
+import Head from 'next/head';
 import Webcam from "react-webcam";
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";
+import { deepStrictEqual } from "assert";
+import { nextTick } from "process";
+import {drawSkeleton, drawKeypoints} from '../../utils/utilities';
 const WebcamComponent = () => <Webcam />;
 
 const videoConstraints = {
@@ -8,27 +14,79 @@ const videoConstraints = {
   facingMode: "user"
 };
 const Home = (props) => {
-    const webcamRef = useRef(null);
-    const capture = useCallback(
-        () => {
-            const imageSrc = webcamRef.current.getScreenshot();
-        },
-        [webcamRef]
-    )
-    return (
-        <div>
-            <Webcam
-                audio={false}
-                height={720}
-                ref={webcamRef}
-                screenshotFormat="image/jpeg"
-                width={1280}
-                videoConstraints={videoConstraints}
-                />
-                <button onClick={capture}> Capture photo </button>
-        </div>
-    )
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
 
+  const detectWebcamFeed = async (posenet_model) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      // Make Estimation
+      const pose = await posenet_model.estimateSinglePose(video);
+      drawResult(pose, video, videoWidth, videoHeight, canvasRef);
+    }
+  };
+  const runPosenet = async () => {
+    const posenet_model = await posenet.load({
+        architecture: 'MobileNetV1',
+        outputStride: 16,
+      inputResolution: { width: 640, height: 480 },
+      multiplier: 0.5
+    });
+    //
+    setInterval(() => {
+      detectWebcamFeed(posenet_model);
+    }, 100);
+  };
+  runPosenet();
+  const drawResult = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext("2d");
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+    drawKeypoints(pose["keypoints"], 0.6, ctx);
+    drawSkeleton(pose["keypoints"], 0.7, ctx);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            width: 640,
+            height: 480
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            width: 640,
+            height: 480
+          }}
+        />
+      </header>
+    </div>
+  );
 }
 
 export default Home;
