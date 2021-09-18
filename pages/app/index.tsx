0ddadfa3a414f4f29e3409c8b6f7545c6bd22580
kensonhui <asimplekevin@gmail.com>
2021-09-17 21:15:12 -0400

    Add webcamera

---
+++ pages/app/index.tsx
@@ -0,0 +1,34 @@
+import {useRef, useCallback } from "react";
+import Webcam from "react-webcam";
+const WebcamComponent = () => <Webcam />;
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
+};
+const Home = (props) => {
+    const webcamRef = useRef(null);
+    const capture = useCallback(
+        () => {
+            const imageSrc = webcamRef.current.getScreenshot();
+        },
+        [webcamRef]
+    )
+    return (
+        <div>
+            <Webcam
+                audio={false}
+                height={720}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                width={1280}
+                videoConstraints={videoConstraints}
+                />
+                <button onClick={capture}> Capture photo </button>
+        </div>
+    )
+
+}
+
+export default Home;
